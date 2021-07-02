8ae44e888d88f100075186191566eb27e80e4a20
Karina Melgoza <kmelgoza08@gmail.com>
2021-07-02 13:48:26 -0700

    adds problem 2 solution

--- sumZero.js
+++ sumZero.js
@@ -19,3 +19,14 @@ let numArr = [1, 2, 3, -2,]
 sumZero(numArr)
 
 //runtime of this code is O(n^2) becuse I am using a nested for loop
+
+function uniqueChar(word) {
+    let string = [...word]
+    return new Set(string).size === string.length
+
+}
+
+uniqueChar('monday')
+
+//runtime of this code is O(n) because the Set object iterates through elements in an array 
+
